5589aeb6f242bbdd5661bcfa00903a4a4b3e951d
Dr Bean <drbean@sac.nuu.edu.tw>
2018-05-26 15:08:13 +0800

    namespace.s/Moodle20/Moodle31/

--- Moosh/Command/Moodle31/Group/GroupingCreate.php
+++ Moosh/Command/Moodle31/Group/GroupingCreate.php
@@ -6,7 +6,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace Moosh\Command\Moodle20\Group;
+namespace Moosh\Command\Moodle31\Group;
 use Moosh\MooshCommand;
 
 class GroupingCreate extends MooshCommand
