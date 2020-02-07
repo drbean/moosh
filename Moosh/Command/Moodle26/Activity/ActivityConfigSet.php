07b86229524d0e96ea1341e10f6f417a856d2f6b
Dr Bean <drbean@sac.nuu.edu.tw>
2020-02-07 13:53:10 +0800

    Moodle23/CourseConfigSet.will_follow_course-config-set

--- Moosh/Command/Moodle26/Activity/ActivityConfigSet.php
+++ Moosh/Command/Moodle26/Activity/ActivityConfigSet.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * moosh - Moodle Shell
  *
@@ -7,99 +6,73 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace Moosh\Command\Moodle26\Activity;
+namespace Moosh\Command\Moodle23\Course;
 use Moosh\MooshCommand;
 
-use GetOptionKit\Argument;
-
-/**
- * Updates an activity
- *
- * @copyright 2013 David Monllaó
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class ActivityConfigSet extends MooshCommand
+class CourseConfigSet extends MooshCommand
 {
-
     public function __construct()
     {
-        parent::__construct('config-set', 'activity');
+        parent::__construct('config-set', 'course');
 
-        $this->addOption('n|name:', 'activity instance name');
-        $this->addOption('s|section:', 'section number', '1');
-        $this->addOption('i|idnumber:', 'idnumber', null);
-        $this->addOption('c|gradecat:', 'gradecategory id', null);
-        $this->addOption('o|options:', 'any options that should be passed for activity creation', null);
-
-        $this->addArgument('coursemoduleid');
-
-        $this->minArguments = 1;
+        $this->addArgument('mode');
+        $this->addArgument('id');
+        $this->addArgument('setting');
+        $this->addArgument('value');
     }
 
-    /**
-     * Uses the data generator to create module instances.
-     *
-     * @return Displays the activity id (not the course module, it depends on the activity type).
-     */
     public function execute()
     {
+        $setting = trim($this->arguments[2]);
+        $value = trim($this->arguments[3]);
 
-        // Getting moodle's data generator.
-        $generator = get_data_generator();
 
-        // All data provided by the data generator.
-/*
-        * @param array|stdClass $record data for module being generated. Requires 'course' key
-        *     (an id or the full object). Also can have any fields from add module form.
-        * @param null|array $options general options for course module. Since 2.6 it is
-        *     possible to omit this argument by merging options into $record
-*/
-        $moduledata = new \stdClass();
-        $moduledata->course = $this->arguments[1];
-        $moduledata->requiresubmissionstatement = 1;
-
-        // $options are course module options.
-        $options = $this->expandedOptions;
-
-        if (!empty($options['options'])) {
-            $course_module_options = preg_split( '/\s+(?=--)/', $options['options']);
-            foreach ( $course_module_options as $option ) {
-                $arg = new Argument( $option );
-                $name = $this->getOptionName($arg);
-                $value = $arg->getOptionValue();
-                $moduledata->$name = $value;
-                if ($this->verbose) {
-                    echo "\"$option\" -> $name=" . $value . "\n";
+        switch ($this->arguments[0]) {
+            case 'course':
+                if(!self::setCourseSetting($this->arguments[1]/* courseid */,$setting,$value)){
+                	// the setting was not applied, exit with a non-zero exit code
+                	cli_error('');
                 }
-            }
+                break;
+            case 'category':
+                //get all courses in category (recursive)
+                $courselist = get_courses($this->arguments[1]/* categoryid */,'','c.id');
+                $succeeded = 0;
+                $failed = 0;
+                foreach ($courselist as $course) {
+                    if(self::setCourseSetting($course->id,$setting,$value)){
+                        $succeeded++;
+                    }else{
+                        $failed++;
+                    }
+                }
+                if($failed == 0){
+                    echo "OK - successfully modified $succeeded courses\n";
+                }else{
+                    echo "WARNING - failed to mofify $failed courses (successfully modified $succeeded)\n";
+                }
+                break;
         }
 
-        if (!empty($options['name'])) {
-            $moduledata->name = $options['name'];
-        }
-        if (!empty($options['idnumber'])) {
-            $moduledata->idnumber = $options['idnumber'];
-        }
-        if (!empty($options['gradecat'])) {
-            $moduledata->gradecat = $options['gradecat'];
-        }
-
-        $moduledata->section = $options['section'];
-
-        $record = $generator->create_module($this->arguments[0], $moduledata);
-
-        if ($this->verbose) {
-            echo "Activity {$this->arguments[0]} created successfully\n";
-        }
-
-        // Return the activity id.
-        echo "{$record->id}\n";
     }
 
-    private function getOptionName($arg)
+    private function setCourseSetting($courseid,$setting,$value) {
+        
+        global $DB;
+        
+        if ($DB->set_field('course',$setting,$value,array('id'=>$courseid))) {
+            echo "OK - Set $setting='$value' (courseid={$courseid})\n";
+            return true;
+        } else {
+            echo "ERROR - failed to set $setting='$value' (courseid={$courseid})\n";
+            return false;
+        }
+
+    }
+
+    protected function getArgumentsHelp()
     {
-        if (preg_match('/^[-]+([_a-zA-Z0-9-]+)/', $arg->arg, $regs)) {
-            return $regs[1];
-        }
+        return "\n\nARGUMENTS:\n\tcourse courseid setting value\n\tOr...\n\tcategory categoryid[all] setting value";
     }
+
 }
