9dbee69ec829f7117ea3f09f775c54e97ceb3b73
Your Name <you@example.com>
2020-11-09 16:38:09 +0800

    case_brace_levels,DB.get_field,for_check

--- Moosh/Command/Moodle23/Course/SectionConfigSet.php
+++ Moosh/Command/Moodle23/Course/SectionConfigSet.php
@@ -66,34 +66,38 @@ class SectionConfigSet extends MooshCommand
             }
             break;
         case 'category':
-                //get all courses in category (recursive)
-                $courselist = get_courses($this->arguments[1]/* categoryid */, '', 'c.id');
-                $succeeded = 0;
-                $failed = 0;
-                foreach ($courselist as $course) {
+            //get all courses in category (recursive)
+            $courselist = get_courses($this->arguments[1]/* categoryid */, '', 'c.id');
+            $succeeded = 0;
+            $failed = 0;
+            foreach ($courselist as $course) {
                 if (!empty($sectionno)) {
                     if (!self::_setSectionSetting($course->id, $sectionno, $setting, $value)) {
                         // the setting was not applied, exit with non-zero exit code
                         cli_error('');
                     }
-            }   else {
+                }
+                else {
                     $course_info = get_fast_modinfo($course->id);
                     $sections = $course_info->get_section_info_all();
                     foreach ($sections as $section) {
-                       if (self::_setSectionSetting($course->id, $section->section, $setting, $value)) {
-                           $succeeded++;
-                       }else{
-                           $failed++;
-                       }
+                        _setSectionSetting($course->id, $section->section, $setting, $value);
+                        $new_value = $DB->get_field('{course_sections}', array('id'=>$activityid), $setting, $value);
+                        if ( $value == $new_value ) {
+                            $succeeded++;
+                        }
+                        else{
+                            $failed++;
+                        }
                     }
                 }
-                if($failed == 0){
-                    echo "OK - successfully modified $succeeded courses\n";
-                }else{
-                    echo "WARNING - failed to mofify $failed courses (successfully modified $succeeded)\n";
-                }
-                break;
             }
+            if($failed == 0){
+                echo "OK - successfully modified $succeeded courses\n";
+            }else{
+                echo "WARNING - failed to mofify $failed courses (successfully modified $succeeded)\n";
+            }
+            break;
         }
     }
 
